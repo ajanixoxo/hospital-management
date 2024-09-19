430a4a307075bdd3730a66df06b6f4d4e3eeb5dc
oscar <denfu.oscar.dev@gmail.com>
2024-09-19 14:01:49 +0000

    style: add default overlay to sheet component

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -56,7 +56,7 @@ const SheetContent = React.forwardRef<
   SheetContentProps
 >(({ side = "right", className, children, ...props }, ref) => (
   <SheetPortal>
-    <SheetOverlay />
+    <SheetOverlay className="fixed inset-0 bg-_gray-200/60 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
     <SheetPrimitive.Content
       ref={ref}
       className={cn(sheetVariants({ side }), className)}
